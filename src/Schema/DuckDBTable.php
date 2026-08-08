4c9669e05d60bb48293ea257c27096e2864168b4
Thomas Bley <mails@thomasbley.de>
2026-08-08 03:39:38 +0200

    cleanup

--- src/Schema/DuckDBTable.php
+++ src/Schema/DuckDBTable.php
@@ -6,6 +6,10 @@ use Doctrine\DBAL\Schema\Column;
 use Doctrine\DBAL\Schema\Table;
 use Doctrine\DBAL\Types\Exception\UnknownColumnType;
 use Doctrine\DBAL\Types\Type;
+use DuckDb\DBAL\Schema\Types\DuckDBMapType;
+use DuckDb\DBAL\Schema\Types\DuckDBStructType;
+use DuckDb\DBAL\Schema\Types\DuckDBType;
+use DuckDb\DBAL\Schema\Types\DuckDBUnionType;
 
 /**
  * A {@see Table} that tolerates unregistered DuckDB type names.
@@ -23,8 +27,15 @@ final class DuckDBTable extends Table /** @phpstan-ignore-line */
             $type = new DuckDBType($type);
         }
 
-        if ($type instanceof DuckDBStructType && isset($options['fields'])) {
-            $type = new DuckDBStructType((string) $options['fields']);
+        if (isset($options['fields'])) {
+            $fields = (string) $options['fields'];
+            if ($type instanceof DuckDBStructType) {
+                $type = new DuckDBStructType($fields);
+            } elseif ($type instanceof DuckDBUnionType) {
+                $type = new DuckDBUnionType($fields);
+            } elseif ($type instanceof DuckDBMapType) {
+                $type = new DuckDBMapType($fields);
+            }
             unset($options['fields']);
         }
 
