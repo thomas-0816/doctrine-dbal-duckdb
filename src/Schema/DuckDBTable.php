d8192adc088a6c399a5fed37f74be86f172ce787
Thomas Bley <mails@thomasbley.de>
2026-08-13 23:02:24 +0200

    cleanup

--- src/Schema/DuckDBTable.php
+++ src/Schema/DuckDBTable.php
@@ -29,4 +29,24 @@ final class DuckDBTable extends Table /** @phpstan-ignore-line */
 
         return $column;
     }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Additionally drops the primary key when it is defined on the dropped column.
+     */
+    public function dropColumn(string $name): self
+    {
+        $primaryKey = $this->getPrimaryKeyConstraint();
+        if ($primaryKey !== null && count($primaryKey->getColumnNames()) === 1) {
+            $primaryKeyColumnName = $this->trimQuotes(strtolower($primaryKey->getColumnNames()[0]->getIdentifier()->getValue()));
+            if ($primaryKeyColumnName === $this->trimQuotes(strtolower($name))) {
+                $this->dropPrimaryKey();
+            }
+        }
+
+        parent::dropColumn($name);
+
+        return $this;
+    }
 }
